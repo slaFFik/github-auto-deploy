d7b9e99ccaf12f79ab41181ac585434992f0e73a
slaFFik <slava.abakumov@gmail.com>
2012-05-30 00:50:45 +0300

    test

---
+++ helpers/debug.php
@@ -0,0 +1,15 @@
+<?php
+
+// for debug
+if(!function_exists('print_var')) {
+    function print_var($var, $die = false){
+        echo '<pre>';
+        if ( !empty($var))
+            print_r($var);
+        else
+            var_dump($var);
+        echo '</pre>';
+        if ($die)
+            die;
+    }
+}
