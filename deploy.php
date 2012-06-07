29dec2e443ebaf26813e377758ca83d8fb0e28f9
slaFFik <slava.abakumov@gmail.com>
2012-06-07 23:30:47 +0300

    cleaning up

--- deploy.php
+++ deploy.php
@@ -71,7 +71,7 @@ class GAD{
         // the main deploy itself
         $this->deploy();
 
-        GAD::log('note', 'Deploy finished'."\r\n".'--------------------');
+        GAD::log('note', 'Deploy finished');
     }
 
     /**
